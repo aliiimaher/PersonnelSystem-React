58ef6b1a6b9d39d303e5ebf40373b078de0e7bd1
aliiiimaher <alimohammadmaher@gmail.com>
2023-12-31 12:00:43 +0330

    user context in the main.tsx handled

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import MyRoutes from "./Routes";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
+import { UserContext } from "./context/UserContext";
+import axios from "axios";
+
 function App() {
+  const [thisUser, setThisUser] = useState(null);
+
+  const fetchUser = async () => {
+    try {
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: "Token " + window.localStorage.getItem("token"),
+      };
+
+      const response = await axios.get("/user/profile", {
+        headers: headers,
+      });
+      setThisUser(response.data);
+    } catch (error) {
+      console.error("Error fetching user data: ", error);
+    }
+  };
+
+  useEffect(() => {
+    window.localStorage.getItem("token") !== null ? fetchUser() : {};
+  }, []);
+
   return (
     <>
-      <div className="app-container" style={{ backgroundColor: "#E7E7E7" }}>
-        <Navbar />
-        <MyRoutes />
-        <Footer />
-      </div>
+      <UserContext.Provider value={thisUser ? thisUser : {}}>
+        <div className="app-container" style={{ backgroundColor: "#E7E7E7" }}>
+          <Navbar />
+          <MyRoutes />
+          <Footer />
+        </div>
+      </UserContext.Provider>
     </>
   );
 }
