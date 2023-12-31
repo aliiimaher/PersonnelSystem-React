f9e0ba4bed56805ac4b38eeba441a2aef8211179
aliiiimaher <alimohammadmaher@gmail.com>
2023-12-31 14:22:50 +0330

    dashboard edited

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Test from "./pages/Test";
 import Login from "./pages/Login";
+import Dashboard from "./pages/userPanel/Dashboard";
 
 function MyRoutes() {
   return (
@@ -11,6 +12,7 @@ function MyRoutes() {
         <Route path="/" element={<HomePage />} />
         <Route path="/t" element={<Test />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Dashboard />} />
       </Routes>
     </Router>
   );
--- src/pages/userPanel/Dashboard.tsx
+++ src/pages/userPanel/Dashboard.tsx
@@ -1,67 +1,79 @@
+import { useContext } from "react";
 import PanelSideBarMenu from "../../components/PanelSideBarMenu";
+import { UserContext } from "../../context/UserContext";
+
+import personSvg from "../../assets/pic/person.svg";
+import sexSvg from "../../assets/pic/panel/SexSvg.svg";
+import callSvg from "../../assets/pic/panel/CallSvg.svg";
+import mailSvg from "../../assets/pic/panel/MailSvg.svg";
+import locationSvg from "../../assets/pic/panel/LocationSvg.svg";
 
 function Dashboard() {
-    
-  return <>{notif && <Notif />}
-  <div className="panel-dashboard">
-    <div className="panel-dashboard-right-side">
-      <PanelSideBarMenu />
-    </div>
-    <div className="panel-dashboard-left-side">
-      <div className="panel-dashboard-personal-info">
-        اطلاعات حساب کاربری
-        <hr />
-        <table>
-          <tr>
-            <th className="dashboard-th">
-              <img src={personSvg} style={{ marginLeft: "8px" }} />
-              <div style={{ fontFamily: "KalamehThin" }}>
-                نام و نام خانوادگی:
-              </div>
-            </th>
-            <td>
-              <strong>
-                {thisUser.first_name} {thisUser.last_name}
-              </strong>
-            </td>
+  const thisUser: any = useContext(UserContext);
+  return (
+    <>
+      {/* {notif && <Notif />} */}
+      <div className="panel-dashboard">
+        <div className="panel-dashboard-right-side">
+          <PanelSideBarMenu />
+        </div>
+        <div className="panel-dashboard-left-side">
+          <div className="panel-dashboard-personal-info">
+            اطلاعات حساب کاربری
+            <hr />
+            <table>
+              <tr>
+                <th className="dashboard-th">
+                  <img src={personSvg} style={{ marginLeft: "8px" }} />
+                  <div style={{ fontFamily: "KalamehThin" }}>
+                    نام و نام خانوادگی:
+                  </div>
+                </th>
+                <td>
+                  <strong>
+                    {thisUser.first_name} {thisUser.last_name}
+                  </strong>
+                </td>
 
-            <th className="dashboard-th">
-              <img src={sexSvg} style={{ marginLeft: "8px" }} />
-              <div style={{ fontFamily: "KalamehThin" }}>جنسیت:</div>
-            </th>
-            <td>
-              <strong>{thisUser.sex}</strong>
-            </td>
-          </tr>
-          <tr>
-            <th className="dashboard-th">
-              <img src={callSvg} style={{ marginLeft: "8px" }} />
-              <div style={{ fontFamily: "KalamehThin" }}>شماره تماس:</div>
-            </th>
-            <td>
-              <strong>{thisUser.phone_number}</strong>
-            </td>
+                <th className="dashboard-th">
+                  <img src={sexSvg} style={{ marginLeft: "8px" }} />
+                  <div style={{ fontFamily: "KalamehThin" }}>جنسیت:</div>
+                </th>
+                <td>
+                  <strong>{thisUser.sex}</strong>
+                </td>
+              </tr>
+              <tr>
+                <th className="dashboard-th">
+                  <img src={callSvg} style={{ marginLeft: "8px" }} />
+                  <div style={{ fontFamily: "KalamehThin" }}>شماره تماس:</div>
+                </th>
+                <td>
+                  <strong>{thisUser.phone_number}</strong>
+                </td>
 
-            <th className="dashboard-th">
-              <img src={mailSvg} style={{ marginLeft: "8px" }} />
-              <div style={{ fontFamily: "KalamehThin" }}>ایمیل:</div>
-            </th>
-            <td>
-              <strong>{thisUser.email}</strong>
-            </td>
-          </tr>
-          <tr>
-            <th className="dashboard-th">
-              <img src={locationSvg} style={{ marginLeft: "8px" }} />
-              <div style={{ fontFamily: "KalamehThin" }}>محل سکونت:</div>
-            </th>
-            <td>
-              <strong>{thisUser.location}</strong>
-            </td>
-          </tr>
-        </table>
+                <th className="dashboard-th">
+                  <img src={mailSvg} style={{ marginLeft: "8px" }} />
+                  <div style={{ fontFamily: "KalamehThin" }}>ایمیل:</div>
+                </th>
+                <td>
+                  <strong>{thisUser.email}</strong>
+                </td>
+              </tr>
+              <tr>
+                <th className="dashboard-th">
+                  <img src={locationSvg} style={{ marginLeft: "8px" }} />
+                  <div style={{ fontFamily: "KalamehThin" }}>محل سکونت:</div>
+                </th>
+                <td>
+                  <strong>{thisUser.location}</strong>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
-    </div>
-  </div></>;
+    </>
+  );
 }
 export default Dashboard;
