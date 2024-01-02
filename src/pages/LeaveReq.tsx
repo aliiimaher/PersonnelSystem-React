d88286475e599901cfc01c4aef2737508fabaf86
aliiiimaher <alimohammadmaher@gmail.com>
2024-01-02 11:17:38 +0330

    middle of imp. of leave req

--- src/pages/LeaveReq.tsx
+++ src/pages/LeaveReq.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import "../styles/pages/LeaveReq.scss";
+import Button from "../components/Button";
 
 function LeaveReq() {
+  const [selectedStartDate, setSelectedStartDate] = useState<Date | null>();
+  const [selectedEndDate, setSelectedEndDate] = useState<Date | null>();
+
+  const testHandle = () => {
+    console.log("start: ", selectedStartDate);
+    console.log("end: ", selectedEndDate);
+  };
   return (
     <>
       <div className="leave-main-container">
@@ -15,9 +26,20 @@ function LeaveReq() {
             <option value="2">معمولی</option>
             <option value="3">اعیاد رسمی</option>
           </select>
-
-          
+          <DatePicker
+            selected={selectedStartDate}
+            onChange={(date) => setSelectedStartDate(date)}
+            dateFormat="yyyy/MM/dd"
+            placeholderText="روز شروع"
+          />
+          <DatePicker
+            selected={selectedEndDate}
+            onChange={(date) => setSelectedEndDate(date)}
+            dateFormat="yyyy/MM/dd"
+            placeholderText="روز پايان"
+          />
         </div>
+        <Button text="دخيره" onclick={testHandle} />
       </div>
     </>
   );
